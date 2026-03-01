f48a088a67cc3fee3fa9931925a5ebaeb36a4653
rajeshbnagaraju <rajeshbnagaraju@users.noreply.github.com>
2026-02-28 22:23:15 -0700

    Configure plugin management and update project name
    Updated root project name and added plugin management for repositories.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,43 @@
-rootProject.name = "boot-offline-test"
+// settings.gradle.kts
+import java.io.File
+
+pluginManagement {
+    repositories {
+        val repoMode =
+            providers.gradleProperty("repoMode").orElse("bootstrap").get()
+
+        val offlineRepoDir =
+            providers.gradleProperty("offlineRepoDir")
+                .orElse("offline-repo")
+                .map { File(rootDir, it).absoluteFile }
+                .get()
+
+        if (repoMode == "consume") {
+            maven { url = uri(offlineRepoDir) }
+        } else {
+            gradlePluginPortal()
+            mavenCentral()
+        }
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        val repoMode =
+            providers.gradleProperty("repoMode").orElse("bootstrap").get()
+
+        val offlineRepoDir =
+            providers.gradleProperty("offlineRepoDir")
+                .orElse("offline-repo")
+                .map { File(rootDir, it).absoluteFile }
+                .get()
+
+        if (repoMode == "consume") {
+            maven { url = uri(offlineRepoDir) }
+        } else {
+            mavenCentral()
+        }
+    }
+}
+
+rootProject.name = "offline-repo-bootstrap"
