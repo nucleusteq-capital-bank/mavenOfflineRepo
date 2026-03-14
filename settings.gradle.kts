9fef00caddec761e9f216b84d681ea0b8a329168
rajeshbnagaraju <rajeshbnagaraju@users.noreply.github.com>
2026-03-13 21:37:34 -0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,43 +1,8 @@
-// settings.gradle.kts
-import java.io.File
-
-pluginManagement {
-    repositories {
-        val repoMode =
-            providers.gradleProperty("repoMode").orElse("bootstrap").get()
-
-        val offlineRepoDir =
-            providers.gradleProperty("offlineRepoDir")
-                .orElse("offline-repo")
-                .map { File(rootDir, it).absoluteFile }
-                .get()
-
-        if (repoMode == "consume") {
-            maven { url = uri(offlineRepoDir) }
-        } else {
-            gradlePluginPortal()
-            mavenCentral()
-        }
-    }
-}
+rootProject.name = "offline-repo-builder"
 
 dependencyResolutionManagement {
     repositories {
-        val repoMode =
-            providers.gradleProperty("repoMode").orElse("bootstrap").get()
-
-        val offlineRepoDir =
-            providers.gradleProperty("offlineRepoDir")
-                .orElse("offline-repo")
-                .map { File(rootDir, it).absoluteFile }
-                .get()
-
-        if (repoMode == "consume") {
-            maven { url = uri(offlineRepoDir) }
-        } else {
-            mavenCentral()
-        }
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
-
-rootProject.name = "offline-repo-bootstrap"
