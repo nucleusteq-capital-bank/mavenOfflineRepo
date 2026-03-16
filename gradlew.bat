5235b03a67520735d0982bdca3e6fa22056f02e8
rajeshbnagaraju <rajeshbnagaraju@users.noreply.github.com>
2026-03-16 07:13:40 -0700

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,93 +1,85 @@
-@rem
-@rem Copyright 2015 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
-@rem
-@rem SPDX-License-Identifier: Apache-2.0
-@rem
+import java.net.URL
 
-@if "%DEBUG%"=="" @echo off
-@rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
-@rem ##########################################################################
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.6" apply false
+    id("io.spring.dependency-management") version "1.1.6" apply false
+}
 
-@rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
+val springBootVersion = "3.5.6"
 
-set DIRNAME=%~dp0
-if "%DIRNAME%"=="" set DIRNAME=.
-@rem This is normally unused
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
 
-@rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-autoconfigure:$springBootVersion")
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
 
-@rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaHome
+    implementation("io.spring.gradle:dependency-management-plugin:1.1.6")
 
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if %ERRORLEVEL% equ 0 goto execute
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+}
 
-echo. 1>&2
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
-echo. 1>&2
-echo Please set the JAVA_HOME variable in your environment to match the 1>&2
-echo location of your Java installation. 1>&2
+tasks.register("buildOfflineRepo") {
 
-goto fail
+    doLast {
 
-:findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+        val repoDir = file("offline-repo")
+        repoDir.mkdirs()
 
-if exist "%JAVA_EXE%" goto execute
+        val configs = listOf(
+            configurations.compileClasspath.get(),
+            configurations.runtimeClasspath.get()
+        )
 
-echo. 1>&2
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
-echo. 1>&2
-echo Please set the JAVA_HOME variable in your environment to match the 1>&2
-echo location of your Java installation. 1>&2
+        configs.forEach { config ->
 
-goto fail
+            config.resolvedConfiguration.resolvedArtifacts.forEach { artifact ->
 
-:execute
-@rem Setup the command line
+                val module = artifact.moduleVersion.id
 
+                val group = module.group
+                val artifactId = module.name
+                val version = module.version
 
+                val groupPath = group.replace(".", "/")
 
-@rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
+                val targetDir = File(repoDir, "$groupPath/$artifactId/$version")
+                targetDir.mkdirs()
 
-:end
-@rem End local scope for the variables with windows NT shell
-if %ERRORLEVEL% equ 0 goto mainEnd
+                val jarFile = File(targetDir, "$artifactId-$version.${artifact.extension}")
 
-:fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c_ return code!
-set EXIT_CODE=%ERRORLEVEL%
-if %EXIT_CODE% equ 0 set EXIT_CODE=1
-if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
-exit /b %EXIT_CODE%
+                artifact.file.copyTo(jarFile, overwrite = true)
 
-:mainEnd
-if "%OS%"=="Windows_NT" endlocal
+                val pomUrl =
+                    "https://repo.maven.apache.org/maven2/$groupPath/$artifactId/$version/$artifactId-$version.pom"
 
-:omega
+                val pomFile = File(targetDir, "$artifactId-$version.pom")
+
+                if (!pomFile.exists()) {
+                    try {
+                        URL(pomUrl).openStream().use { input ->
+                            pomFile.outputStream().use { output ->
+                                input.copyTo(output)
+                            }
+                        }
+                    } catch (e: Exception) {
+                        println("Missing POM for $group:$artifactId:$version")
+                    }
+                }
+
+                println("Added $group:$artifactId:$version")
+            }
+        }
+
+        println("")
+        println("Offline repo created at:")
+        println(repoDir.absolutePath)
+    }
+}
